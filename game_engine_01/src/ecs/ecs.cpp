2d621834fce2627d8e6db8aacec10d5e57660cab
JGeanca <geancarlorivera831@gmail.com>
2024-09-03 10:30:09 -0600

    System methods implemented

--- game_engine_01/src/ecs/ecs.cpp
+++ game_engine_01/src/ecs/ecs.cpp
@@ -1,5 +1,24 @@
 #include "ecs.hpp"
 
+#include <algorithm>
+
 int IComponent::nextId = 0;
 
 int Entity::getId() const { return id; }
+
+// System methods
+void System::addEntityFromSystem(const Entity& entity) {
+  entities.push_back(entity);
+}
+void System::removeEntityFromSystem(const Entity& entity) {
+  auto it = std::remove_if(
+      entities.begin(), entities.end(),
+      [&entity](const Entity& other) { return entity == other; });
+  entities.erase(it, entities.end());
+}
+
+std::vector<Entity>& System::getEntities() { return entities; }
+
+const signature& System::getComponentSignature() const {
+  return componentSignature;
+}
--- game_engine_01/src/ecs/ecs.hpp
+++ game_engine_01/src/ecs/ecs.hpp
@@ -1,10 +1,14 @@
 #ifndef ECS_HPP
 #define ECS_HPP
 
+#include <bitset>
 #include <memory>
 #include <vector>
 
 #include "../utils/pool.hpp"
+
+const unsigned int MAX_COMPONENTS = 64;
+typedef std::bitset<MAX_COMPONENTS> signature;
 
 struct IComponent {
  protected:
@@ -27,7 +31,35 @@ class Entity {
  public:
   Entity(int id) : id(id) {}
   int getId() const;
+
+  bool operator==(const Entity& other) const { return id == other.id; }
+  bool operator!=(const Entity& other) const { return id != other.id; }
+  bool operator<(const Entity& other) const { return id < other.id; }
+  bool operator>(const Entity& other) const { return id > other.id; }
 };
+
+class System {
+ private:
+  signature componentSignature;
+  std::vector<Entity> entities;
+
+ public:
+  System() = default;
+  ~System() = default;
+  void addEntityFromSystem(const Entity& entity);
+  void removeEntityFromSystem(const Entity& entity);
+  std::vector<Entity>& getEntities();
+  const signature& getComponentSignature() const;
+
+  template <typename TComponent>
+  void requireComponent();
+};
+
+template <typename TComponent>
+void System::requireComponent() {
+  const int componentId = Component<TComponent>::getId();
+  componentSignature.set(componentId);
+}
 
 class Register {
  private:
