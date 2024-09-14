7059e458354ef586bb8245adc5ba095134b0efbb
JGeanca <geancarlorivera831@gmail.com>
2024-09-14 13:14:20 -0600

    Lua intro finished

--- lua_test/scripts/script01.lua
+++ lua_test/scripts/script01.lua
@@ -10,7 +10,17 @@ config = {
   },
 }
 
+function factorial(n)
+  if n == 0 then
+    return 1
+  else
+    return n * factorial(n - 1)
+  end
+end
+
+var_pow = pow(2,4)
 
 print("[LUA] script01.lua loaded")
 print("[LUA] var_name = " .. var_name)
 print("[LUA] var_age = " .. var_age)
+print("[LUA] var_pow = " .. var_pow)
--- lua_test/src/main.cpp
+++ lua_test/src/main.cpp
@@ -2,9 +2,18 @@
 #include <sol/sol.hpp>
 #include <string>
 
+int Pow(int x, int y) {
+  int result = 1;
+  for (int i = 0; i < y; i++) {
+    result *= x;
+  }
+  return result;
+}
+
 void lua_test() {
   sol::state lua;
   lua.open_libraries(sol::lib::base);
+  lua["pow"] = Pow;
 
   try {
     lua.script_file("./scripts/script01.lua");
@@ -18,6 +27,10 @@ void lua_test() {
     std::cout << "[C++] config.title: " << title << std::endl;
     bool fullscreen = config["fullscreen"];
     std::cout << "[C++] config.fullscreen: " << fullscreen << std::endl;
+
+    sol::function factorial = lua["factorial"];
+    int result = factorial(5);
+    std::cout << "[C++] factorial(5): " << result << std::endl;
   } catch (const sol::error& e) {
     std::cerr << "Lua error: " << e.what() << std::endl;
   }
